b77be4e72cda6530534d5b666320170bbdae785f
Christian Jaime Bazan Giron <christian_jaime_18@hotmail.com>
2026-04-14 00:51:10 -0500

    proyecto inicial

---
+++ src/main/java/com/sesiones/sesiones_backend/SesionesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sesiones.sesiones_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SesionesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SesionesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sesiones/sesiones_backend/SesionesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sesiones.sesiones_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SesionesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
